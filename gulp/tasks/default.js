112c9886fed735478364eb32d892cc2ee51bc92e
Zernyu Chou <swiftdemise@gmail.com>
2015-04-08 16:13:34 -0500

    Fixed cleaning build file sync issue. Added semantic-ui

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var runSequence = require('run-sequence');
 var connect = require('gulp-connect');
 var del = require('del');
 var react = require('gulp-react');
@@ -9,7 +10,10 @@ var errorHandler = require('../util/handleErrors');
 var paths = {
   html: ['src/html/**/*.html'],
   scripts: ['src/js/**/*.js'],
-  libraries: ['bower_components/**/*.js']
+  libraries: [
+    'bower_components/react/*.js',
+    'bower_components/semantic-ui/dist/semantic.css'
+  ]
 };
 
 gulp.task('clean', function (cb) {
@@ -24,24 +28,32 @@ gulp.task('connect', function () {
 });
 
 gulp.task('html', function () {
-  gulp.src(paths.html)
+  return gulp.src(paths.html)
       .pipe(handleErrors(errorHandler))
       .pipe(gulp.dest('build'))
       .pipe(connect.reload());
 });
 
 gulp.task('scripts', function () {
-  gulp.src(paths.scripts)
+  return gulp.src(paths.scripts)
       .pipe(handleErrors(errorHandler))
       .pipe(react())
       .pipe(gulp.dest('build/js'))
       .pipe(connect.reload());
 });
 
-gulp.task('build', ['clean', 'html', 'scripts'], function () {
-  gulp.src(paths.libraries)
+gulp.task('libraries', function () {
+  return gulp.src(paths.libraries)
       .pipe(handleErrors(errorHandler))
-      .pipe(gulp.dest('build/js'));
+      .pipe(gulp.dest('build/libs'));
+});
+
+gulp.task('build', function (cb) {
+  runSequence(
+      'clean',
+      ['html', 'scripts', 'libraries'],
+      cb
+  );
 });
 
 gulp.task('watch', function () {
