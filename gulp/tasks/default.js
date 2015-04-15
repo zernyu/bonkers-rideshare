4896c21b90448406c2a8129776f3ef1a4649e689
Zernyu Chou <swiftdemise@gmail.com>
2015-04-15 16:58:19 -0500

    Now supporting production build mode. Huzzah!

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -10,10 +10,10 @@ var errorHandler = require('../util/handleErrors');
 var substituter = require('gulp-substituter');
 
 // JS build tools
-var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var reactify = require('reactify');
+var uglifyify = require('uglifyify');
 var watchify = require('watchify');
 var streamify = require('gulp-streamify');
 
@@ -43,10 +43,6 @@ var paths = {
   }
 };
 
-gulp.task('clean', function (cb) {
-  del(['build/**/*'], cb);
-});
-
 gulp.task('connect', function () {
   connect.server({
     root: paths.build.root,
@@ -55,8 +51,17 @@ gulp.task('connect', function () {
 });
 
 gulp.task('html', function () {
+  var appPaths = [paths.build.root + '/css/*.css'];
+  if (process.env.environment === 'production') {
+    gutil.log(gutil.colors.yellow('Building in Production mode'));
+    appPaths.push(paths.build.dist + '/' + paths.build.minified);
+  } else {
+    gutil.log(gutil.colors.yellow('Building in Development mode'));
+    appPaths.push(paths.build.src + '/' + paths.build.out);
+  }
+
   var libSources = gulp.src([paths.build.src + '/libs/**/*.js', paths.build.src + '/libs/**/*.css'], {read: false});
-  var appSources = gulp.src([paths.build.src + '/app.js', paths.build.root + '/css/*.css'], {read: false});
+  var appSources = gulp.src(appPaths, {read: false});
 
   return gulp.src(paths.html)
       .pipe(handleErrors(errorHandler))
@@ -76,14 +81,6 @@ gulp.task('libraries', function () {
   return gulp.src(paths.libraries, {base: './bower_components'})
       .pipe(handleErrors(errorHandler))
       .pipe(gulp.dest(paths.build.src + '/libs'));
-});
-
-gulp.task('build', function (cb) {
-  runSequence(
-      'clean',
-      'html',
-      cb
-  );
 });
 
 gulp.task('watch', ['style', 'libraries'], function () {
@@ -115,21 +112,27 @@ gulp.task('watch', ['style', 'libraries'], function () {
       .pipe(gulp.dest(paths.build.src));
 });
 
-gulp.task('build//TODO', function () {
-  browserify({
+gulp.task('clean', function (cb) {
+  del([paths.build.dist + '/**/*'], cb);
+});
+
+gulp.task('build', ['clean'], function () {
+  return browserify({
     entries: [paths.scripts.entry],
-    transform: [reactify]
+    transform: [reactify, uglifyify]
   })
       .bundle()
       .pipe(source(paths.build.minified))
-      .pipe(substituter(config.parse))
       .pipe(streamify(substituter(config.parse)))
-      .pipe(streamify(uglify(paths.build.minified)))
       .pipe(gulp.dest(paths.build.dist));
-
-  // TODO: gulp.task('production', ['replaceHTML', 'build']); where replaceHTML injects the built js path
 });
 
 gulp.task('default', function () {
+  process.env.environment = 'development';
   sequence('watch', 'html', 'connect');
+});
+
+gulp.task('production', function () {
+  process.env.environment = 'production';
+  sequence('build', 'html');
 });
