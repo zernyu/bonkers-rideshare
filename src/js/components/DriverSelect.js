1b67b25af9697c7c743965dc474f7028de81f73a
Zernyu Chou <swiftdemise@gmail.com>
2015-04-17 02:07:47 -0500

    Case sensitive cleanup

---
+++ src/js/components/DriverSelect.js
@@ -0,0 +1,20 @@
+var React = require('react/addons');
+
+var DriverSelect = React.createClass({
+  onChange: function() {
+    this.props.valueLink.requestChange(this.getDOMNode().value);
+  },
+
+  render: function () {
+    return (
+        <select onChange={this.onChange} value={this.props.valueLink.value}>
+          <option value="">I need a ride!</option>
+          {this.props.options.map(function (driver) {
+            return <option key={'driver' + driver.objectId} value={driver.objectId}>{driver.name}</option>;
+          })}
+        </select>
+    );
+  }
+});
+
+module.exports = DriverSelect;
