fe09e9ae6076cef6cbb4a945031c9de2bdd73aa8
Zernyu Chou <swiftdemise@gmail.com>
2015-05-02 18:10:52 -0500

    Auto build number incrementing

--- native/hooks/before_prepare/timestamp_version_bump.js
+++ native/hooks/before_prepare/timestamp_version_bump.js
@@ -5,25 +5,6 @@ var fs = require("fs"),
 
 var rootdir = process.argv[2];
 if (rootdir) {
-
-  module.exports = function(context) {
-
-    var cordova_util = context.requireCordovaModule("cordova-lib/src/cordova/util"),
-        ConfigParser = context.requireCordovaModule("cordova-lib/src/configparser/ConfigParser"),
-        platforms = context.requireCordovaModule("cordova-lib/src/cordova/platforms"),
-        projectRoot = cordova_util.isCordova(),
-        xml = cordova_util.projectConfig(projectRoot),
-        cfg = new ConfigParser(xml);
-
-    var getProjectFile = function(platform, relPath) {
-      var platform_path = path.join(projectRoot, "platforms", platform);
-      var parser = new platforms[platform].parser(platform_path);
-      if (typeof parser.cordovaproj !== "undefined") {
-        return path.join(parser.cordovaproj, relPath);
-      }
-      return path.join(parser.path, relPath);
-    };
-
     var replace = function(path, to_replace, replace_with) {
       var data = fs.readFileSync(path, "utf8");
       if (data.indexOf(replace_with) <= -1) {
@@ -42,26 +23,7 @@ if (rootdir) {
       return year.toString() + month.toString() + day.toString() + hours.toString() + min.toString();
     })();
 
-    var updateIOSCFBundleVersion = function() {
-      var plist = getProjectFile("ios", cfg.name() + "-Info.plist");
-      var shell = require(path.join(projectRoot, "platforms/ios/cordova/node_modules/shelljs"));
-      shell.exec("/usr/libexec/PlistBuddy -c \"Set :CFBundleVersion " + timestamp + "\" \"" + plist + "\"");
-    };
-
-    var updateAndroidVersionCode = function() {
-      var manifest = getProjectFile("android", "AndroidManifest.xml");
-      var version_code = "android:versionCode=\"" + timestamp + "\"";
-      replace(manifest, "android:versionCode=\"[0-9^\"]*\"", version_code);
-    };
-
-    context.opts.platforms.forEach(function(platform) {
-      if (platform === "ios") {
-        updateIOSCFBundleVersion();
-      } else if (platform === "android") {
-        updateAndroidVersionCode();
-      }
-    });
-
-  };
-
+    var config = rootdir + '/config.xml';
+    replace(config, 'android-versionCode=\"[0-9]*\"', 'android-versionCode="' + timestamp + '"');
+    replace(config, 'ios-CFBundleVersion=\"[0-9]*\"', 'ios-CFBundleVersion="' + timestamp + '"');
 }
